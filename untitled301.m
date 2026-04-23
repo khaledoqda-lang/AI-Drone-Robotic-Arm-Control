7bd278a6e18296233effb93e04d451fbe814599f
khaledoqda-lang <khaledoqda@std.mans.edu.eg>
2026-04-23 22:41:39 +0200

    Add files via upload

---
+++ untitled301.m
@@ -0,0 +1,14 @@
+close all
+clear all
+clc
+sim=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
+    sim.simxFinish(-1); % just in case, close all opened connections
+    clientID=sim.simxStart('127.0.0.1',19999,true,true,5000,5);
+if (clientID>-1)
+        disp('Connected to remote API server');
+        else
+        disp('Failed connecting to remote API server');
+    end
+    sim.delete(); % call the destructor!
+    
+    disp('Program ended');
